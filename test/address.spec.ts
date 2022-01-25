7f8b495d608d90cdd90e9b0322cbe4ba9ed0ca02
Shinigami92 <chrissi92@hotmail.de>
2022-01-25 18:05:04 +0100

    test: use a random generated seed for random-tests

--- test/address.spec.ts
+++ test/address.spec.ts
@@ -336,7 +336,10 @@ describe('address', () => {
     });
   }
 
-  describe('non seed-based tests', () => {
+  // Create and log-back the seed for debug purposes
+  faker.seed(Math.ceil(Math.random() * 1_000_000_000));
+
+  describe(`random seeded tests for seed ${faker.seedValue}`, () => {
     for (let i = 1; i <= NON_SEEDED_BASED_RUN; i++) {
       describe('countryCode()', () => {
         it('returns random alpha-3 countryCode', () => {
